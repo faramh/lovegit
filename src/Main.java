97cd70b982aa91d0e3317146f6b1166728b1940d
faramh <85996539+faramh@users.noreply.github.com>
2021-07-15 12:41:21 +0430

    Delete src directory

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-        int a,b;
-        System.out.println("git doost");
-    }
-
-}
--- src/Myclass.java
+++
@@ -1,7 +0,0 @@
-public class Myclass {
-    int value ;
-
-    public Myclass(int value) {
-        this.value = value;
-    }
-}
